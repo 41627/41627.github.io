6c60fd941b56efecbc6be51a47208871e8082968
ownphys <zan94928@gmail.com>
2025-10-05 23:44:21 +0800

    Update clusters.js

--- clusters.js
+++ clusters.js
@@ -1,8 +1,9 @@
 // 集群数据
 export const clusters = [
-    { roots: [0], nodes: [0, 1, 2, 3], url: "http://localhost:8080/4l1.html" },
-    { roots: [4], nodes: [4, 5, 6, 7], url: "http://localhost:8080/4l.html" },
-    { roots: [10], nodes: [8, 9, 10], url: "http://localhost:8080/4l1%20copy.html" },
-    { roots: [13], nodes: [11, 12, 13], url: "file:///D:/desk/%E7%8E%B0%E4%BB%A3/4l/onesideforce.html" },
-    { roots: [15], nodes: [14, 15], url: "file:///D:/desk/%E7%8E%B0%E4%BB%A3/fixed_d3.html" }
+    { roots: [0], nodes: [0, 1, 2, 3], url: "https://www.ownphys.com/cluster1" },
+    { roots: [4], nodes: [4, 5, 6, 7], url: "https://www.ownphys.com/cluster2" },
+    { roots: [10], nodes: [8, 9, 10], url: "https://www.ownphys.com/cluster3" },
+    { roots: [13], nodes: [11, 12, 13], url: "https://www.ownphys.com/cluster4" },
+    { roots: [15], nodes: [14, 15], url: "https://www.ownphys.com/cluster5" }
+
 ];
