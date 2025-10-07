eee58b86a40d1b3190b19b38f9dda94ad10941ef
ownphys <zan94928@gmail.com>
2025-10-07 18:41:12 +0800

    Update clusters.js

--- clusters.js
+++ clusters.js
@@ -1,9 +1,10 @@
 // 集群数据
 export const clusters = [
-    { roots: [0], nodes: [0, 1, 2, 3], url: "https://www.ownphys.com/cluster1" },
-    { roots: [4], nodes: [4, 5, 6, 7], url: "https://www.ownphys.com/cluster2" },
-    { roots: [10], nodes: [8, 9, 10], url: "https://www.ownphys.com/cluster3" },
-    { roots: [13], nodes: [11, 12, 13], url: "https://www.ownphys.com/cluster4" },
-    { roots: [15], nodes: [14, 15], url: "https://www.ownphys.com/cluster5" }
+    { roots: 0, nodes: [0, 1, 2, 3], url: "https://www.ownphys.com/cluster1" },
+    { roots: 4, nodes: [4, 5, 6, 7], url: "https://www.ownphys.com/cluster2" },
+    { roots: 10, nodes: [8, 9, 10], url: "https://www.ownphys.com/cluster3" },
+    { roots: 13, nodes: [11, 12, 13], url: "https://www.ownphys.com/cluster4" },
+    { roots: 15, nodes: [14, 15], url: "https://www.ownphys.com/cluster5" }
 
 ];
+
